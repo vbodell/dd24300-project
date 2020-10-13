d6288e2ebbda4bcc460a0344ea2b4115456d1c64
Victor Bodell <vbodell@kth.se>
2020-10-13 21:22:03 +0200

    Add prefix to script output

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
-echo "Install packages"
+echo "SCRIPT: Install packages"
 sudo apt-get update -q
 sudo apt-get install -q git wget unzip -y
 sudo apt-get install -q python python3-pip -y
 
-echo "Update pip"
+echo "SCRIPT: Update pip"
 curl -s https://bootstrap.pypa.io/get-pip.py | sudo python3
 sudo pip install -q --upgrade setuptools
 
@@ -16,23 +16,23 @@ APIKEY=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attrib
 GITHUB_PWD=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/GITHUB_PWD -H Metadata-Flavor:Google)
 FETCH_AND_PROCESS_DATASET=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/FETCH_AND_PROCESS_DATASET -H Metadata-Flavor:Google)
 
-echo "Using env vars APIKEY=$APIKEY, GITHUB_PWD=$GITHUB_PWD, FETCH_AND_PROCESS_DATASET=$FETCH_AND_PROCESS_DATASET"
+echo "SCRIPT: Using env vars APIKEY=$APIKEY, GITHUB_PWD=$GITHUB_PWD, FETCH_AND_PROCESS_DATASET=$FETCH_AND_PROCESS_DATASET"
 
 # Get github repo
-echo "Get repo"
+echo "SCRIPT: Get repo"
 git clone https://carpool-master:$GITHUB_PWD@github.com/ErikBavenstrand/covid19.git
 cd covid19
 sudo pip install -q -r requirements.txt
 
 if $FETCH_AND_PROCESS_DATASET; then
-    echo "Get Egypt Dataset file, can take a couple of minutes..."
+    echo "SCRIPT: Get Egypt Dataset file, can take a couple of minutes..."
     wget -q https://md-datasets-cache-zipfiles-prod.s3.eu-west-1.amazonaws.com/8h65ywd2jr-3.zip
-    echo "Unzipping dataset. Will probably also take a little while..."
+    echo "SCRIPT: Unzipping dataset. Will probably also take a little while..."
     sudo -q unzip 8h65ywd2jr-3.zip
     sudo -q unzip COVID-19\ Dataset.zip
     sudo rm 8h65ywd2jr-3.zip COVID-19\ Dataset.zip
 
-    echo "Generate the tfrecords"
+    echo "SCRIPT: Generate the tfrecords"
     sudo python3 generate.py
 fi
 
